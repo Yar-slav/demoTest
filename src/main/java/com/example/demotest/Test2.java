c593f2cbb90f0e710748d9011bbc355e8cd1b898
yfedyna <yfedyna@griddynamics.com>
2023-05-18 11:11:32 +0300

    test3

--- src/main/java/com/example/demotest/Test2.java
+++ src/main/java/com/example/demotest/Test2.java
@@ -3,5 +3,6 @@ package com.example.demotest;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Test2");
+        System.out.println("Test3");
     }
 }
