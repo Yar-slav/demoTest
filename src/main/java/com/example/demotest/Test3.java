fdd7e266518e4934ab4e1c6a3c2a55f9386bc739
yfedyna <yfedyna@griddynamics.com>
2023-05-18 11:31:01 +0300

    branch2 2

--- src/main/java/com/example/demotest/Test3.java
+++ src/main/java/com/example/demotest/Test3.java
@@ -3,5 +3,6 @@ package com.example.demotest;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("branch2");
+        System.out.println("branch2");
     }
 }
