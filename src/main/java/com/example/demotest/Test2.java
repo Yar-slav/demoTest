43d2293358746098199238142f133e08261b5a3f
yfedyna <fedynayaroslav@gmail.com>
2023-05-24 16:58:49 +0300

    test commit 12

--- src/main/java/com/example/demotest/Test2.java
+++ src/main/java/com/example/demotest/Test2.java
@@ -2,12 +2,13 @@ package com.example.demotest;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Test2");
+        System.out.println("Test12");
         System.out.println("Test3");
         System.out.println("Test4");
         System.out.println("Test5");
         System.out.println("Test6");
         System.out.println("Test7");
         System.out.println("Test99");
+        System.out.println("Test12");
     }
 }
