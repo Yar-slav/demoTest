e04eb19d3c5b77db61d0ce64aef3bba17c84b5d5
Yar-slav <101950948+Yar-slav@users.noreply.github.com>
2023-05-24 17:09:06 +0300

    Update Test2.java

--- src/main/java/com/example/demotest/Test2.java
+++ src/main/java/com/example/demotest/Test2.java
@@ -8,7 +8,7 @@ public class Test2 {
         System.out.println("Test5");
         System.out.println("Test6");
         System.out.println("Test7");
-        System.out.println("Test99");
-        System.out.println("Test12");
+        System.out.println("Test9");
+        System.out.println("Test1");
     }
 }
