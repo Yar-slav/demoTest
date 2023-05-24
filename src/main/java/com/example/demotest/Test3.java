ebe1d3970ed79607423599395b6da0ce4ebad37f
yfedyna <fedynayaroslav@gmail.com>
2023-05-24 17:04:28 +0300

    test commit 33

--- src/main/java/com/example/demotest/Test3.java
+++ src/main/java/com/example/demotest/Test3.java
@@ -4,6 +4,6 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("branch2");
         System.out.println("branch2");
-        System.out.println("branc33");
+        System.out.println("branch33");
     }
 }
