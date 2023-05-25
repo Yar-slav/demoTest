cc32cf2a8d59999507b9c46896f6d1c71eadae86
yfedyna <fedynayaroslav@gmail.com>
2023-05-25 14:20:09 +0300

    test 3

--- src/main/java/com/example/demotest/Test3.java
+++ src/main/java/com/example/demotest/Test3.java
@@ -4,6 +4,6 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("branch2");
         System.out.println("branch2");
-        System.out.println("branch33");
+        System.out.println("branch333");
     }
 }
