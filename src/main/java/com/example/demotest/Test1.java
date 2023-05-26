cd7aea49e2b8f0a79a0f5653da7c803d76f45594
yfedyna <fedynayaroslav@gmail.com>
2023-05-26 11:57:11 +0300

    test111

--- src/main/java/com/example/demotest/Test1.java
+++ src/main/java/com/example/demotest/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test11");
+        System.out.println("Test111");
     }
 }
