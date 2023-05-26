b137085f2e8fcb15355050e062e764a7ea0bfc13
yfedyna <fedynayaroslav@gmail.com>
2023-05-26 11:56:26 +0300

    test2

--- src/main/java/com/example/demotest/Test1.java
+++ src/main/java/com/example/demotest/Test1.java
@@ -3,5 +3,6 @@ package com.example.demotest;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
+        System.out.println("Test2");
     }
 }
