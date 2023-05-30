8ce03b0fcf6e15ef9a75d932fd905946285ae962
yfedyna <fedynayaroslav@gmail.com>
2023-05-30 14:55:33 +0300

    test11

--- src/main/java/com/example/demotest/Test1.java
+++ src/main/java/com/example/demotest/Test1.java
@@ -3,10 +3,12 @@ package com.example.demotest;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test4");
+        System.out.println("Test6");
         System.out.println("Test11");
         System.out.println("Test113");
         System.out.println("Test111");
-        System.out.println("Test111");
+        System.out.println("Test131");
+        System.out.println("Test131");
         System.out.println("Test111");
     }
 }
