32d37c06f1b2c6c69b94b7967a191535789bb57a
yfedyna <fedynayaroslav@gmail.com>
2023-06-01 12:58:42 +0300

    log

--- src/main/java/com/example/demotest/Test1.java
+++ src/main/java/com/example/demotest/Test1.java
@@ -1,7 +1,11 @@
 package com.example.demotest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Test1 {
     public static void main(String[] args) {
+        log.info("start");
         System.out.println("Test4");
         System.out.println("Test6");
         System.out.println("Test11");
@@ -10,10 +14,13 @@ public class Test1 {
         System.out.println("Test131");
         System.out.println("Test131");
         System.out.println("Test111");
+        log.info("finish");
     }
 
     public void testT(){
+        log.info("start");
         System.out.println("fdfdf");
         System.out.println("fdfdf");
+        log.info("start");
     }
 }
