ea09e67fa4cb3a6f5a363cb344c3679087bcd694
yfedyna <fedynayaroslav@gmail.com>
2023-06-01 12:52:10 +0300

    test2

--- src/main/java/com/example/demotest/Test1.java
+++ src/main/java/com/example/demotest/Test1.java
@@ -1,5 +1,10 @@
 package com.example.demotest;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test4");
